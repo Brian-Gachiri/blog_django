20fb55abd91f03303bf259aa097e1f83bd954a20
unknown <brigeveriz7@gmail.com>
2021-09-03 09:49:44 +0300

    Created base template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+
+    context = {
+        'heading' : 'My first Blog!',
+        'services' : ["Future Fridays", "Mondays with You", "Oh the wekend"],
+        'posts' : ["Big Blue world", "Out of space", "A-O A-Okay"],
+    }
+
+    return render(request,"home.html", context )
+
+def contact(request):
+
+    context = {
+        'number' : '0707320000',
+        'email' : 'briangachiri@gmail.com',
+        'address': 'somewhere',
+        'services' : ["Future Fridays", "Mondays with You", "Oh the weekend"],
+
+    }
+
+    return render(request,"contact.html", context )
+
+def blog(request):
+
+    context = {
+        'posts' : ["Big Blue world", "Out of space", "A-O A-Okay", "I am number 4", "Worcesteshire Alley", "Salt & Sugar"],
+
+    }
+
+    return render(request,"blog/blog.html", context )
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home" ),
+    path('contact', views.contact, name="contact"),
+    path('blog', views.blog, name="blog")
+
 ]
